671e73a2c7fe30d805a96633de41298f17f5b295
Daniel Alejo <daniel-alejo94@hotmail.com>
2023-03-18 21:53:06 -0600

    view articles logout expired token

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, SignIn, SignUp, ProtectedRoutes, NewArticle } from "./views";
+import {
+  Home,
+  SignIn,
+  SignUp,
+  ProtectedRoutes,
+  NewArticle,
+  ArticleById,
+} from "./views";
 import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg-gray-900 min-h-screen">
+      <div className="bg-gray-900 min-h-screen pb-1">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/article/:articleId" element={<ArticleById />} />
           <Route element={<ProtectedRoutes />}>
             <Route path="/newarticle" element={<NewArticle />} />
           </Route>
--- src/views/index.js
+++ src/views/index.js
@@ -3,5 +3,6 @@ import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import ProtectedRoutes from "./ProtectedRoutes";
 import NewArticle from "./NewArticle";
+import ArticleById from "./ArticleById";
 
-export { Home, SignIn, SignUp, ProtectedRoutes, NewArticle };
+export { Home, SignIn, SignUp, ProtectedRoutes, NewArticle, ArticleById };
